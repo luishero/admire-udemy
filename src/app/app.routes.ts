da7b5e547dac44973ab50ddfc42e901b02b75135
luis <luishero@yahoo.com>
2018-01-21 23:09:56 +0100

    Primer commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,45 @@
+import { RouterModule, Routes } from '@angular/router'; // agregamos el RouterModule
+
+import { PagesComponent } from './pages/pages.component';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
+import { RegisterComponent } from './login/register.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+
+
+
+
+
+
+
+const appRoutes: Routes = [
+    { path: '',
+     component: PagesComponent, // con espacio vacio estas va a PaCo, rutas trabajan con el router-outlet secundario
+    children: [
+
+        { path: 'dashboard', component: DashboardComponent},
+        { path: 'progress', component: ProgressComponent },
+        { path: 'graficas1', component: Graficas1Component },
+        { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+
+
+
+
+    ]}, // cualqier ruta nos lleva a PagesComponent
+
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: NopagefoundComponent }
+];
+
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } ); // forRoot para las rutas principales, usamos el hash
+//  cuando lleva la palabra Module va siempre en imports[]
+
