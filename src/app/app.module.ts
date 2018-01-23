abb491103190fecc0aa864859d5c5f1afcdfaadb
luis <luishero@yahoo.com>
2018-01-23 11:32:32 +0100

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTES } from './app.routes';
+// import { PagesComponent } from './pages/pages.component';
 
+// Modulos
+import { PagesModule } from './pages/pages.module'; // se registra en imports
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-
-// para usar las rutas
-import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
+
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { ProgressComponent } from './pages/progress/progress.component';
+// import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
+// import { HeaderComponent } from './shared/header/header.component';
+// import { SidebarComponent } from './shared/sidebar/sidebar.component';
+// import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+// import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
 
 
 
@@ -25,19 +31,23 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    ProgressComponent,
-    PagesComponent,
+
+    // DashboardComponent,
+    // Graficas1Component,
+    // ProgressComponent,
+    // PagesComponent,
+
+    // NopagefoundComponent,
+    // HeaderComponent,
+    // SidebarComponent,
+    // BreadcrumbsComponent,
+
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { RouterModule, Routes } from '@angular/router'; // agregamos el RouterMo
 
 import { PagesComponent } from './pages/pages.component';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { ProgressComponent } from './pages/progress/progress.component';
+// import { Graficas1Component } from './pages/graficas1/graficas1.component';
+ import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 import { RegisterComponent } from './login/register.component';
 import { LoginComponent } from './login/login.component';
@@ -20,20 +20,17 @@ import { LoginComponent } from './login/login.component';
 
 
 const appRoutes: Routes = [
-    { path: '',
-     component: PagesComponent, // con espacio vacio estas va a PaCo, rutas trabajan con el router-outlet secundario
-    children: [
+    // { path: '',
+    //  component: PagesComponent, // con espacio vacio estas va a PaCo, rutas trabajan con el router-outlet secundario
+    // children: [
 
-        { path: 'dashboard', component: DashboardComponent},
-        { path: 'progress', component: ProgressComponent },
-        { path: 'graficas1', component: Graficas1Component },
-        { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    //     { path: 'dashboard', component: DashboardComponent},
+    //     { path: 'progress', component: ProgressComponent },
+    //     { path: 'graficas1', component: Graficas1Component },
+    //     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 
-
-
-
-
-    ]}, // cualqier ruta nos lleva a PagesComponent
+    //             ]
+    // }, // cualqier ruta nos lleva a PagesComponent
 
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
